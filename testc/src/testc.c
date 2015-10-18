955501e42123cc58a22d24a861f97358deea22d3
Daniel Las <lasdaniel@gmail.com>
2015-10-18 13:25:09 +0200

    Rozne printf

--- testc/src/testc.c
+++ testc/src/testc.c
@@ -12,7 +12,21 @@
 #include <stdlib.h>
 
 int main(void) {
-	printf("1*2=%f", 1.0 * 2);
+	char str[] = "Ciąg znaków w zmiennej";
+	char * str_ptr = &str;
+	int num = 10;
+	int * num_ptr = &num;
+
+	printf("Ciąg znaków: %s\n", "Statyczy ciąg znaków");
+	printf("Ciąg znaków w zmiennej: %s\n", str);
+	printf("Ciąg znaków w zmiennej przez wskaźnik: %s\n", str_ptr);
+	str_ptr++;
+	printf("Ciąg znaków w zmiennej przez wskaźnik++: %s\n", str_ptr);
+	printf("Liczba zmiennoprzecinkowa: 1.5*2.7=%f\n", 1.5 * 2.7);
+	printf("Liczba całkowita w zmiennej num: %i\n", num);
+	printf("Liczba całkowita w zmiennej num_ptr: %i\n", *num_ptr);
+	printf("Adres zmiennej num: %i\n", &num);
+	printf("Na co wskazuje num_ptr: %i\n", num_ptr);
 
 	return EXIT_SUCCESS;
 }
